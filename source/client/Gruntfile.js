916f387612c0a0395857e7ee47e7d4b7fd41ab34
Mythli <github@mythli.net>
2014-02-01 00:41:45 +0100

    initial project structure

---
+++ source/client/Gruntfile.js
@@ -0,0 +1,106 @@
+﻿module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				files: {
+					'build/css.css' : 'style/design.scss'
+				}
+			}
+		},
+
+		emberTemplates: {
+			compile: {
+				options: {
+					templateFileExtensions: /\.hbs/,
+					templateBasePath: /template\//
+				},
+				files: {
+					'build/template.js': 'template/**/*.hbs'
+				}
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					'build/style.css': [
+						'build/css.css'
+					]
+				}
+			}
+		},
+		uglify: {
+			build: {
+				files: {
+					'build/Script.js': [
+					],
+					'build/compatibility.js': [
+						'script/library/html5shiv.js',
+						'script/library/Respond.js'
+					]
+				}
+			}
+		},
+
+		compress: {
+			main: {
+				options: {
+					mode: 'gzip',
+					level: 6
+				},
+				
+				files: [
+					{ expand: true, src: ['build/**/*.js'], dest: '', ext: '.js.gz' },
+					{ expand: true, src: ['build/**/*.css'], dest: '', ext: '.css.gz' }
+				]
+			}
+		},
+
+		clean: [
+			'build/**/*'
+		],
+
+		watch: {
+			compileHandlebars: {
+				files: [
+					'template/**/*.hbs'
+				],
+				tasks: [
+					'emberTemplates'
+				]
+			},
+			compileSaas: {
+				files: [
+					'style/**/*.scss'
+				],
+				tasks: [
+					'sass'
+				]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-ember-templates');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', [
+		'clean',
+		'sass',
+		'emberTemplates',
+		'cssmin',
+		'uglify',
+		'compress'
+	]);
+
+	grunt.registerTask('debug', [
+		'emberTemplates',
+		'sass',
+		'watch'
+	]);
+};
